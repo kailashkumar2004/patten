264f0291bcd2783d888c7bb903fe1afe2633dfa0
kailash kumar <kailashkumartkg@gmail.com>
2024-02-08 13:02:03 +0530

    initial commit

---
+++ patten.js
@@ -0,0 +1,123 @@
+// var i = 1;
+// var a = 10;
+// while (i <= a) {
+//     var j = 1;
+//     var String = "";
+//     while (j <= i) {
+//         String += "*";
+//         j=j+1
+//     }
+//     i = i + 1;
+//     console.log(String)
+// };
+
+// var i = 10;
+// var a = 1;
+// while (i >= a) {
+//     var j = 1;
+//     var String = "";
+//     while (j <= i) {
+//         String += "*";
+//         j=j+1
+//     }
+//     i = i - 1;
+//     console.log(String)
+// };
+
+// var i = 1;
+// var x = 15;
+// while (i <= x) {
+//     var j = 1;
+//     var String = "";
+//     while (j <= x) {
+//         String += "*";
+//         j=j+1
+//     }
+//     i = i + 1;
+//     console.log(String)
+// };
+
+// var i = 1;
+// var num1 = 15;
+// while (i <= num1) {
+//     var j = 1;
+//     var String = "";
+//     while (j <= i) {
+//         String += "*";
+//         j=j+1
+//     }
+//     i = i + 1;
+//     console.log(String)
+// };
+// var i = 14;
+// var num1 = 1;
+// while (i >= num1) {
+//     var j = 1;
+//     var String = "";
+//     while (j <= i) {
+//         String += "*";
+//         j=j+1
+//     }
+//     i = i - 1;
+//     console.log(String)
+// };
+
+// var i = 20;
+// var num1 = 1;
+// while (i >= num1) {
+//     var j = 1;
+//     var String = "";
+//     while (j <= i) {
+//         String += "*";
+//         j=j+1
+//     }
+//     i = i - 1;
+//     console.log(String)
+// };
+// var i = 2;
+// var num1 = 20;
+// while (i <= num1) {
+//     var j = 1;
+//     var String = "";
+//     while (j <= i) {
+//         String += "*";
+//         j=j+1
+//     }
+//     i = i + 1;
+//     console.log(String)
+// };
+
+// var i = 1;
+// var a = 15;
+// while (i <= a) {
+//     var j = 1;
+//     var String = "";
+//     while (j <= a) {
+//         String += "*";
+//         j=j+1
+//     }
+//     i = i + 1;
+//     console.log(String)
+// };
+// var i = 1;
+// var num1 = 20;
+// while (i <= num1) {
+//     var j = 1;
+//     var String = "";
+//     while (j <= num1) {
+//         String += "*";
+//         j=j+1
+//     }
+//     i = i + 1;
+//     console.log(String)
+// };
+
+// var i = 1;
+// var a = 10;
+// var String = "";
+// while (i <= a) {
+//     String = String + i;
+//     console.log(String);
+//     i=i+1
+// };
+
